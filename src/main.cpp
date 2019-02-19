9e4e671abb5cfbcdb8b98159af649ea37f292c3e
THOMAS-LAPTOP\thoma_000 <thomasbeckstrom@gmail.com>
2019-02-19 14:41:57 -0600

    getting leds to work

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 // #include <serstream>
 #include <math.h>
 #include <FastLED.h>
-#define NUM_STRIPS 6
+// #define NUM_STRIPS 6
 
 #define PACMAN_RADIUS 36 // radius in inches
 #define PACMAN_MOUTH_STEP 5 // additional leds to step for mouth animation
@@ -24,29 +24,30 @@ const int PACMAN_RAYS_LEDS = (int) PACMAN_RADIUS*0.0254*30;
 #define STRIP_6_NUM_LEDS PACMAN_RAYS_LEDS
 
 // #define NUM_LEDS STRIP_1_NUM_LEDS + STRIP_2_NUM_LEDS + STRIP_3_NUM_LEDS + STRIP_4_NUM_LEDS + STRIP_5_NUM_LEDS + STRIP_6_NUM_LEDS
-const int NUM_LEDS = STRIP_1_NUM_LEDS + STRIP_2_NUM_LEDS + STRIP_3_NUM_LEDS + STRIP_4_NUM_LEDS + STRIP_5_NUM_LEDS + STRIP_6_NUM_LEDS;
-#define STRIP_1_PIN 5
+// const int NUM_LEDS = STRIP_1_NUM_LEDS + STRIP_2_NUM_LEDS + STRIP_3_NUM_LEDS + STRIP_4_NUM_LEDS + STRIP_5_NUM_LEDS + STRIP_6_NUM_LEDS;
+const int NUM_LEDS = PACMAN_OUTLINE_LEDS + PACMAN_RAYS_LEDS*2;
+#define STRIP_1_PIN 8
 #define STRIP_2_PIN 6
-#define STRIP_3_PIN 7
-#define STRIP_4_PIN 8
+#define STRIP_3_PIN 10
+#define STRIP_4_PIN 5
 #define STRIP_5_PIN 9
-#define STRIP_6_PIN 10
+#define STRIP_6_PIN 7
 
-#define BRIGHTNESS  255
-#define FRAMES_PER_SECOND  8
+#define BRIGHTNESS 200
+#define FRAMES_PER_SECOND 5
 
-CRGBPalette16 currentPalette( PartyColors_p );
+// CRGBPalette16 currentPalette( PartyColors_p );
 uint8_t       colorLoop = 1;
 
 //FOR FIRE
-bool gReverseDirection = false;
-CRGBPalette16 gPal;
+// bool gReverseDirection = false;
+// CRGBPalette16 gPal;
 
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 uint8_t rHue = 0; // for controlling rainbow sweep
-uint8_t chomp_frame = 0;
+int chomp_frame = 0;
 
 CRGB outline_frames[3][5] = {
   { CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Black },
@@ -68,15 +69,16 @@ CRGB ray_frames[3][2] = {
 // CRBG ray_frames[2] = { CRGB::Black, CRGB::Black };
 
 int animation_frame = 0;
+bool opening = true;
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, STRIP_1_PIN>(leds, 0, STRIP_1_NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, STRIP_2_PIN>(leds, 0, STRIP_2_NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, STRIP_3_PIN>(leds, PACMAN_OUTLINE_LEDS, STRIP_3_NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, STRIP_4_PIN>(leds, PACMAN_OUTLINE_LEDS, STRIP_4_NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, STRIP_5_PIN>(leds, PACMAN_OUTLINE_LEDS + PACMAN_RAYS_LEDS, STRIP_5_NUM_LEDS);
-  FastLED.addLeds<NEOPIXEL, STRIP_6_PIN>(leds, PACMAN_OUTLINE_LEDS + PACMAN_RAYS_LEDS, STRIP_6_NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_1_PIN>(leds, 0, PACMAN_OUTLINE_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_2_PIN>(leds, 0, PACMAN_OUTLINE_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_3_PIN>(leds, PACMAN_OUTLINE_LEDS, PACMAN_RAYS_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_4_PIN>(leds, PACMAN_OUTLINE_LEDS, PACMAN_RAYS_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_5_PIN>(leds, PACMAN_OUTLINE_LEDS + PACMAN_RAYS_LEDS, PACMAN_RAYS_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP_6_PIN>(leds, PACMAN_OUTLINE_LEDS + PACMAN_RAYS_LEDS, PACMAN_RAYS_LEDS);
   printf(NUM_LEDS);
   // x = random16();
   // y = random16();
@@ -85,13 +87,9 @@ void setup() {
 
   FastLED.setMaxPowerInVoltsAndMilliamps(5,4500);
 
-  gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White); // fire color palette
-  }
-
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
 // typedef void (*SimplePatternList[])();
 // SimplePatternList gPatterns = { juggle_plus_mirrored ,rainbow, confetti, sine_rainbow_fwd_glitter, Fire2012, bpm, rainbowWithGlitter, Fire2012WithPalette};
+}
 
 void chomp(int chomp_frame) {
 
@@ -134,6 +132,19 @@ void loop() {
   // EVERY_N_MILLISECONDS( 10 ) { animation_frame++; } // slowly cycle the "base color" through the rainbow
   chomp(animation_frame);
   FastLED.show();
+
+  if (animation_frame == 0) {
+    opening = true;
+  } else if (animation_frame == 2) {
+    opening = false;
+  }
+
+  if (opening == true) {
+    animation_frame++;
+  } else {
+    animation_frame--;
+  }
+
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND);
 }
